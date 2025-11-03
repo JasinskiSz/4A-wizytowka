2f9bd4c0fe96cd0e1124eed68fab4346173c8360
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-11-03 12:39:00 +0100

    przygotowanie pod wpisywanie drugiej wartości

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function generuj() {
     console.log("button click")
-    const imie = document.getElementById("imie-input").value
-    console.log(imie)
 
-    document.getElementById("wynik-generowania").innerHTML = imie
+    const imie = document.getElementById("imie-input").value
+    const nazwisko = document.getElementById("nazwisko-input").value
+
+    console.log(imie)
+    console.log(nazwisko)
+
+    document.getElementById("wynik-generowania-imie").innerHTML = imie
+    document.getElementById("wynik-generowania-nazwisko").innerHTML = nazwisko
 }
